1c65e8e9bb1d314d52d51cf1af0fa292e3bc1122
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-09-30 05:39:44 +0300

    completred

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { directors } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+
+
+ function Directors() {
+ return (
+ <div>
+ <h1>Directors Page</h1>
+ {directors.map((director) => (
+   <div key={director.name}>
+     <h3>{director.name}</h3>
+     <ul>
+       {director.movies.map((movie) => (
+         <li key={movie}>{movie}</li>
+       ))}
+     </ul>
+   </div>
+ ))}
+</div>
+);
+  }
+
 
 export default Directors;
